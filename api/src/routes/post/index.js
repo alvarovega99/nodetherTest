4d391d0a7786c9067aab3f7ab646e78de90095a6
Alvaro Vega <64806626+alvarovega99@users.noreply.github.com>
2022-08-27 21:00:53 -0300

    put

--- api/src/routes/post/index.js
+++ api/src/routes/post/index.js
@@ -12,8 +12,8 @@ const {
 router.post("/create-post", createPostController);
 router.get("/search-one-post/:id", searchOnePost);
 router.get("/search-all-post", searchAllPost);
-router.get("/update-like/:id", updateLike);
-router.get("/update-dislike/:id", updateDislike);
+router.put("/update-like/:id", updateLike);
+router.put("/update-dislike/:id", updateDislike);
 router.delete("/delete-post/:id", deletePostController);
 
 module.exports = router;
